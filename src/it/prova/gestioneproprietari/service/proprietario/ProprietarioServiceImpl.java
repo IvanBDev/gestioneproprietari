8721e95ebf2f23d6f6ad859fbb78e19d354868f4
VaXinNavi <ivan_bendotti@yahoo.com>
2022-05-20 09:28:48 +0200

    Patch 1.1: Implementato metodo

--- src/it/prova/gestioneproprietari/service/proprietario/ProprietarioServiceImpl.java
+++ src/it/prova/gestioneproprietari/service/proprietario/ProprietarioServiceImpl.java
@@ -3,15 +3,39 @@ package it.prova.gestioneproprietari.service.proprietario;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
+import it.prova.gestioneproprietari.dao.EntityManagerUtil;
 import it.prova.gestioneproprietari.dao.proprietario.ProprietarioDAO;
 import it.prova.gestioneproprietari.model.Proprietario;
 
 public class ProprietarioServiceImpl implements ProprietarioService{
+	
+	private ProprietarioDAO proprietarioDAO;
+	
+	public void setProprietarioDAO(ProprietarioDAO proprietarioDAO) {
+		this.proprietarioDAO = proprietarioDAO;
+	}
 
 	@Override
 	public List<Proprietario> listAllProprietari() throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		// questo è come una connection
+				EntityManager entityManager = EntityManagerUtil.getEntityManager();
+
+				try {
+					// uso l'injection per il dao
+					proprietarioDAO.setEntityManager(entityManager);
+
+					// eseguo quello che realmente devo fare
+					return proprietarioDAO.list();
+
+				} catch (Exception e) {
+					e.printStackTrace();
+					throw e;
+				} finally {
+					EntityManagerUtil.closeEntityManager(entityManager);
+				}
 	}
 
 	@Override
@@ -42,12 +66,6 @@ public class ProprietarioServiceImpl implements ProprietarioService{
 	public List<Proprietario> trovaProprietariConAutomobileImmatricolataDa(Date dataInput) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
-	}
-
-	@Override
-	public void setProprietarioDAO(ProprietarioDAO proprietarioDAO) throws Exception {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
