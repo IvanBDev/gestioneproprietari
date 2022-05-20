6f2d6cdb411a67b8a847ee1b43940603e37375f2
VaXinNavi <ivan_bendotti@yahoo.com>
2022-05-20 09:21:15 +0200

    Patch 1.0: Implementato metodo

--- src/it/prova/gestioneproprietari/dao/proprietario/ProprietarioDAOImpl.java
+++ src/it/prova/gestioneproprietari/dao/proprietario/ProprietarioDAOImpl.java
@@ -8,11 +8,17 @@ import javax.persistence.EntityManager;
 import it.prova.gestioneproprietari.model.Proprietario;
 
 public class ProprietarioDAOImpl implements ProprietarioDAO{
+	
+	private EntityManager entityManager;
+	
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
 
 	@Override
 	public List<Proprietario> list() throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		return entityManager.createQuery("FROM Proprietario", Proprietario.class).getResultList();
 	}
 
 	@Override
@@ -35,12 +41,6 @@ public class ProprietarioDAOImpl implements ProprietarioDAO{
 
 	@Override
 	public void delete(Proprietario proprietario) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setEntityManager(EntityManager entityManager) {
 		// TODO Auto-generated method stub
 		
 	}
