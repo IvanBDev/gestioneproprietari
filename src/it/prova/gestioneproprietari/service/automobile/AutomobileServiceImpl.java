ff788efb2b206353d787e852e9869859df0ad9f2
VaXinNavi <ivan_bendotti@yahoo.com>
2022-05-20 12:20:03 +0200

    Patch 4.4: Implementato metodo update (Automobile)

--- src/it/prova/gestioneproprietari/service/automobile/AutomobileServiceImpl.java
+++ src/it/prova/gestioneproprietari/service/automobile/AutomobileServiceImpl.java
@@ -93,9 +93,29 @@ public class AutomobileServiceImpl implements AutomobileService {
 	}
 
 	@Override
-	public void rimuovi(Automobile automobile) throws Exception {
+	public void rimuovi(Automobile automobileInstance) throws Exception {
 		// TODO Auto-generated method stub
+		// questo è come una connection
+		EntityManager entityManager = EntityManagerUtil.getEntityManager();
 
+		try {
+			// questo è come il MyConnection.getConnection()
+			entityManager.getTransaction().begin();
+
+			// uso l'injection per il dao
+			automobileDAO.setEntityManager(entityManager);
+
+			// eseguo quello che realmente devo fare
+			automobileDAO.update(automobileInstance);
+
+			entityManager.getTransaction().commit();
+		} catch (Exception e) {
+			entityManager.getTransaction().rollback();
+			e.printStackTrace();
+			throw e;
+		} finally {
+			EntityManagerUtil.closeEntityManager(entityManager);
+		}
 	}
 
 	@Override
